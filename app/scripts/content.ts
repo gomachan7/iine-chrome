ac8afabb994b145f02a1a6059e49b1bd89c6a393
e_ntyo <entyo.contact@gmail.com>
2018-02-11 18:24:50 +0900

    [WIP] とりあえずDOMで動く

--- app/scripts/content.ts
+++ app/scripts/content.ts
@@ -1,5 +1,22 @@
+const nicoJS = require('nicojs');
+
+if (!document.getElementById('iine-container')) {
+  const container = document.createElement('div')
+  container.setAttribute("id", "iine-container");
+  document.body.appendChild(container);
+}
+
+const container = document.getElementById('iine-container');
+console.log(container);
+const nico = new nicoJS({
+  app       : container,
+  width     : 600,
+  height    : 400,
+  font_size : 50,     // opt
+  color     : '#000'  // opt
+});
+nico.listen();
+
 document.addEventListener("mousewheel", () => {
-  const div = document.createElement("div");
-  document.body.appendChild(div);
-  div.innerText = "test123";
+  nico.send('いいね♡');
 });
