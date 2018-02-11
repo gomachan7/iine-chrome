aff78dc0540714a6c46d78f1e7bc08dc182e91b6
e_ntyo <entyo.contact@gmail.com>
2018-02-12 02:11:35 +0900

    :sparkles: できた

--- app/scripts/content.ts
+++ app/scripts/content.ts
@@ -1,22 +1,13 @@
-const nicoJS = require('nicojs');
-
-if (!document.getElementById('iine-container')) {
-  const container = document.createElement('div')
-  container.setAttribute("id", "iine-container");
-  document.body.appendChild(container);
-}
-
-const container = document.getElementById('iine-container');
-console.log(container);
-const nico = new nicoJS({
-  app       : container,
-  width     : 600,
-  height    : 400,
-  font_size : 50,     // opt
-  color     : '#000'  // opt
-});
-nico.listen();
-
-document.addEventListener("mousewheel", () => {
-  nico.send('いいね♡');
-});
+document.body.onclick = (evt: MouseEvent) => {
+  const span = document.createElement('span');
+  span.textContent = 'いいね♡';
+  span.style.position = 'absolute';
+  span.style.top = evt.pageY + 'px';
+  span.classList.add('iine');
+  span.addEventListener('webkitAnimationEnd', () => {
+    if (span.parentNode != null) {
+      span.parentNode.removeChild(span);
+    }
+  });
+  document.body.appendChild(span);
+};
